3b4c3fd9d94f8827bc73f1fbce400de2df2b28f6
Madyan Eka <madyan.eka95@gmail.com>
2018-10-18 08:50:18 +0700

    using <transition-group> (preparations)

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,17 @@
                 <transition name="fade" mode="out-in">
                     <component :is="selectedComponent"></component>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="addItem">Add Item</button>
+                <br><br>
+                <ul class="list-group">
+                    <li
+                        class="list-group-item"
+                        v-for="(number, index) in numbers"
+                        :key="index"
+                        @click="removeItem(index)"
+                        style="cursor: pointer">{{ number }}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -68,7 +79,8 @@
                 load: true,
                 alertAnimation: 'fade',
                 elementWidth: 100,
-                selectedComponent: 'app-success-alert'
+                selectedComponent: 'app-success-alert',
+                numbers: [1, 2, 3, 4, 5]
             }
         },
 
@@ -119,6 +131,15 @@
             },
             leaveCancelled(el) {
                 console.log('leaveCancelled')
+            },
+
+            addItem() {
+                const position = Math.floor(Math.random() * this.numbers.length)
+                this.numbers.splice(position, 0, this.numbers.length + 1)
+            },
+
+            removeItem(index) {
+                this.numbers.splice(index, 1)
             }
         },
 
